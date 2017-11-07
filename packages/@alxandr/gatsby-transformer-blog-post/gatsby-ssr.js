1ce03effb8a1b3da8c91145f08b244ac3fdd35bf
Aleksander Heintz <alxandr@alxandr.me>
2017-11-07 20:26:39 +0100

    Improve stuffs

--- packages/@alxandr/gatsby-transformer-blog-post/gatsby-ssr.js
+++ packages/@alxandr/gatsby-transformer-blog-post/gatsby-ssr.js
@@ -1,32 +1,32 @@
-const React = require('react');
-const { renderToString } = require('react-dom/server');
-const { JssProvider, SheetsRegistry } = require('react-jss');
-const CleanCSS = require('clean-css');
+// const React = require('react');
+// const { renderToString } = require('react-dom/server');
+// const { JssProvider, SheetsRegistry } = require('react-jss');
+// const CleanCSS = require('clean-css');
 
-const cssCleaner = new CleanCSS({});
+// const cssCleaner = new CleanCSS({});
 
-exports.replaceRenderer = ({
-  bodyComponent,
-  replaceBodyHTMLString,
-  setHeadComponents,
-}) => {
-  const sheets = new SheetsRegistry();
+// exports.replaceRenderer = ({
+//   bodyComponent,
+//   replaceBodyHTMLString,
+//   setHeadComponents,
+// }) => {
+//   const sheets = new SheetsRegistry();
 
-  const bodyHTML = renderToString(
-    <JssProvider registry={sheets}>
-      {bodyComponent}
-    </JssProvider>,
-  );
+//   const bodyHTML = renderToString(
+//     <JssProvider registry={sheets}>
+//       {bodyComponent}
+//     </JssProvider>,
+//   );
 
-  replaceBodyHTMLString(bodyHTML);
-  const css = sheets.toString();
-  const minCss = cssCleaner.minify(css).styles;
-  setHeadComponents([
-    <style
-      type="text/css"
-      id="server-side-jss"
-      key="server-side-jss"
-      dangerouslySetInnerHTML={{ __html: minCss }}
-    />,
-  ]);
-};
+//   replaceBodyHTMLString(bodyHTML);
+//   const css = sheets.toString();
+//   const minCss = cssCleaner.minify(css).styles;
+//   setHeadComponents([
+//     <style
+//       type="text/css"
+//       id="server-side-jss"
+//       key="server-side-jss"
+//       dangerouslySetInnerHTML={{ __html: minCss }}
+//     />,
+//   ]);
+// };
